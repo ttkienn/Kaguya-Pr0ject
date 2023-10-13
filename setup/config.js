844c9958d84d2f11606ec24dca22a82e289d70b6
Thiệu Trung Kiên <87187870+ttkienn@users.noreply.github.com>
2023-10-13 16:16:15 +0700

    Update config.js

--- setup/config.js
+++ setup/config.js
@@ -8,7 +8,8 @@ export default {
         "listenTyping": false,
         "logLevel": "silent",
         "updatePresence": true,
-        "selfListen": true
+        "selfListen": true,
+        "userAgent": "Mozilla/5.0 (Linux; Android 9; SM-G973U Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36",
     },
     database: {
         type: "json", // json or mongodb
